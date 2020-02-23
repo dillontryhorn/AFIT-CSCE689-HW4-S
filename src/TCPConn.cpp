e7aa77597b290b24e9d7958275383ffe064ba3a2
dillontryhorn <dillontryhorn@gmail.com>
2020-02-23 12:34:56 -0500

    Added conversion

--- src/TCPConn.cpp
+++ src/TCPConn.cpp
@@ -323,7 +323,8 @@ void TCPConn::serverHandshake() {
       }
 
       std::vector<uint8_t> new_buf(buf.begin(), buf.begin()+32);
-      std::string checkStr(buf.begin()+32, buf.end());
+      std::vector<uint8_t> encryptedStr(buf.begin()+32, buf.end());
+      std::string checkStr(encryptedStr.begin(), encryptedStr.end());
       decryptData(checkStr);
 
       if(checkStr.compare(_rand_handshake) != 0)
