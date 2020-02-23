477d2bdbcfa07ddf727717d62b1164be42fc8965
dillontryhorn <dillontryhorn@gmail.com>
2020-02-23 11:32:51 -0500

    Added verbosity statement so that I can see the two strings being compared.

--- src/TCPConn.cpp
+++ src/TCPConn.cpp
@@ -329,6 +329,8 @@ void TCPConn::serverHandshake() {
       if(checkStr.compare(_rand_handshake) != 0)
       {
          std::stringstream msg;
+         if(_verbosity >= 3)
+            msg << checkStr << " does not match " << _rand_handshake << "\n";
          msg << "Handshake failed. Random strings do not match. Cannot authenticate.";
          _server_log.writeLog(msg.str().c_str());
          disconnect();
@@ -372,6 +374,8 @@ void TCPConn::transmitData() {
       if(checkStr.compare(_rand_handshake) != 0)
       {
          std::stringstream msg;
+         if(_verbosity >= 3)
+            msg << checkStr << " does not match " << _rand_handshake << "\n";
          msg << "Handshake failed. Random strings do not match. Cannot authenticate.";
          _server_log.writeLog(msg.str().c_str());
          disconnect();
